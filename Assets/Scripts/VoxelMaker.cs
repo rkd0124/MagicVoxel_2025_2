e5142fc0db1684cc95360063c34705249ca24446
fefef <fef>
2025-09-15 11:08:54 +0900

    지동생성(안되는데?)

--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -10,6 +10,9 @@ public class VoxelMaker : MonoBehaviour
 
     public static List<GameObject> voxelPool = new List<GameObject>();
 
+    public float creatTime = 0.1f;
+
+    float currentTime = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -17,7 +20,6 @@ public class VoxelMaker : MonoBehaviour
         for (int i = 0; i < voxelPoolSize; i++)
         {
             GameObject voxel = Instantiate(voxleFactory);
-
             voxel.SetActive(false);
 
             voxelPool.Add(voxel);
@@ -27,23 +29,28 @@ public class VoxelMaker : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Fire1"))  //사용자가 버튼을 누르면
+        currentTime += Time.deltaTime;
+
+        if (currentTime > creatTime)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hitinfo = new RaycastHit();
-
-            if (Physics.Raycast(ray, out hitinfo)) //마우스 클릭한 곳이 바닥이면
+            if (Input.GetButtonDown("Fire1"))  //사용자가 버튼을 누르면
             {
-                if (voxelPool.Count > 0)
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hitinfo = new RaycastHit();
+
+                if (Physics.Raycast(ray, out hitinfo)) //마우스 클릭한 곳이 바닥이면
                 {
-                    GameObject voxel = voxelPool[0]; //풀의 최상단꺼 가져옴
-                    voxel.SetActive(true); //그거 활성화
+                    if (voxelPool.Count > 0)
+                    {
+                        currentTime = 0;
 
-                    voxel.transform.position = hitinfo.point; //클릭한 위치에 생성
-                    voxelPool.RemoveAt(0); //풀 하나 삭제
-                    
+                        GameObject voxel = voxelPool[0]; //풀의 최상단꺼 가져옴
+                        voxel.SetActive(true); //그거 활성화
+
+                        voxel.transform.position = hitinfo.point; //클릭한 위치에 생성
+                        voxelPool.RemoveAt(0); //풀 하나 삭제
+                    }
                 }
-
             }
         }
     }
